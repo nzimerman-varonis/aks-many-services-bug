16f1627c831424d1a0be0afe8534eb4f7c3569bd
Nimrod Zimerman <nzimerman@varonis.com>
2023-08-13 08:04:33 +0300

    Use $KUBECTL in ssh-node.sh script

--- ssh-node.sh
+++ ssh-node.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+source config.sh
+
 NODE_NAME=$1
 
 if [ "$NODE_NAME" == "" ] ; then
@@ -10,22 +12,22 @@ if [ "$NODE_NAME" == "" ] ; then
 fi
 
 # Get one of the Linux nodes
-LINUX_NODE=$(kubectl get nodes | grep system | head -1 | gawk '{ print $1 }')
+LINUX_NODE=$($KUBECTL get nodes | grep system | head -1 | gawk '{ print $1 }')
 
 # Start a debug pod on the Linux node, without attaching to it
-DEBUG_POD=$(kubectl debug node/$LINUX_NODE -ti --attach=false --image=alpine | gawk '{ print $4 }')
+DEBUG_POD=$($KUBECTL debug node/$LINUX_NODE -ti --attach=false --image=alpine | gawk '{ print $4 }')
 
 # Wait for debug pod to become ready
-kubectl wait pod $DEBUG_POD --for condition=Ready --timeout=10s
+$KUBECTL wait pod $DEBUG_POD --for condition=Ready --timeout=10s
 
 # Install openssh and sshpass in the debug pod
-kubectl exec -ti $DEBUG_POD -- /bin/sh -c "apk update && apk add openssh sshpass"
+$KUBECTL exec -ti $DEBUG_POD -- /bin/sh -c "apk update && apk add openssh sshpass"
 
 # Get target node IP from node name
-NODE_IP=$(kubectl get node $NODE_NAME -o jsonpath="{.status.addresses[0].address}")
+NODE_IP=$($KUBECTL get node $NODE_NAME -o jsonpath="{.status.addresses[0].address}")
 
 # SSH into a Windows node, using password from file
-kubectl exec -ti $DEBUG_POD -- sshpass -p $(cat windows-password.txt) ssh azureuser@${NODE_IP} -o StrictHostKeyChecking=no
+$KUBECTL exec -ti $DEBUG_POD -- sshpass -p $(cat windows-password.txt) ssh azureuser@${NODE_IP} -o StrictHostKeyChecking=no -o ServerAliveInterval=15 -o ServerAliveCountMax=3
 
 # Delete the debug pod
-kubectl delete pod $DEBUG_POD --wait=false
+$KUBECTL delete pod $DEBUG_POD --wait=false
